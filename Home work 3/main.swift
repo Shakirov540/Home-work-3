a9a6e87b6baf495080f6e6997e463fc91c8713f7
Aibek <ashakirov981@gmail.com>
2022-09-17 23:18:46 +0600

    No comments

--- Home work 3/main.swift
+++ Home work 3/main.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
+//N1
 
-
+func snowSeason(num: Int) {
+    if num == 1{
+        print("17.09.22:18Gradysov")
+    }else if num == 2{
+        print("18.09.22:28gradysov")
+    }else if num == 3{
+        print("19.09.22:29gradysov")
+    }else{
+       return
+        }
+}
